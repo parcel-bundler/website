e027ee6552eac27bd5de04154e7e69ebafffc5f0
James Hegedus <jthegedus@jttech.io>
2018-08-24 16:40:00 +1000

    fix shellcheck error
    specifically:
    > [shellcheck] Double quote to prevent globbing and word splitting. [SC2086]

--- build.sh
+++ build.sh
@@ -5,10 +5,10 @@ mkdir dist
 
 for lang in src/i18n/*; do
   rm -rf tmp;
-  cp -R $lang tmp;
+  cp -R "$lang" tmp;
   cp -R src/assets tmp/layout/assets;
-  ./node_modules/.bin/generate-md --layout tmp/layout --input tmp/docs --output "dist/$(basename $lang)";
-  cp $lang/index.html "dist/$(basename $lang)/index.html";
+  ./node_modules/.bin/generate-md --layout tmp/layout --input tmp/docs --output "dist/$(basename "$lang")";
+  cp "$lang/index.html" "dist/$(basename "$lang")/index.html";
 done
 
 rm -rf tmp
