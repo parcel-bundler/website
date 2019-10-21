8456807343fbff0b92dbecead9402d7addb677e4
Allen Hai <allenhai03@gmail.com>
2019-10-21 11:16:31 -0500

    fix subdomain levels to x.x.x from x.x.x.x

--- build.sh
+++ build.sh
@@ -9,6 +9,10 @@ for lang in src/i18n/*; do
   cp -R src/assets tmp/layout/assets;
   ./node_modules/.bin/generate-md --layout tmp/layout --input tmp/docs --output "dist/$(basename "$lang")";
   cp "$lang/index.html" "dist/$(basename "$lang")/index.html";
+  echo "{
+    \"version\": 2,
+    \"alias\": [\"$(basename "$lang")-parceljs.coetry.now.sh\"]
+}" >> "dist/$(basename "$lang")/now.json"
 done
 
 rm -rf tmp
