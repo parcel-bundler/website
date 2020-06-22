d9d4824bb538f35132c3e6093290e34f4bc4103f
Allen Hai <allen@zeit.co>
2020-06-22 12:10:06 -0500

    Move Parcel to ZEIT Now v2 (#537)

--- build.sh
+++ build.sh
@@ -9,6 +9,17 @@ for lang in src/i18n/*; do
   cp -R src/assets tmp/layout/assets;
   ./node_modules/.bin/generate-md --layout tmp/layout --input tmp/docs --output "dist/$(basename "$lang")";
   cp "$lang/index.html" "dist/$(basename "$lang")/index.html";
+  if [ "$lang" = "en" ]; then
+    echo "{
+      \"version\": 2,
+      \"alias\": [\"$(basename "$lang").parceljs.org\", \"parceljs.org\"]
+    }" >> "dist/$(basename "$lang")/now.json";
+    else echo "{
+      \"version\": 2,
+      \"alias\": [\"$(basename "$lang").parceljs.org\"]
+    }" >> "dist/$(basename "$lang")/now.json";
+  fi;
+  cat "dist/$(basename "$lang")/now.json";
 done
 
 rm -rf tmp
---
+++ entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/sh -l
+
+set -eu # stop on error
+node /usr/src/app/deploy.js 
