aa84016e1d5104d6031533d9d88f226ebbf7e719
toastal <toastal@protonmail.com>
2021-02-26 20:41:38 +0700

    Use ISO date format in DOM; user prefence in UI

---
+++ src/_includes/replace-date.js
@@ -0,0 +1,29 @@
+"use strict";
+(function () {
+  const replaceElements = document.querySelectorAll(
+    "time[datetime].js-replace-date"
+  );
+
+  if (replaceElements.length === 0) {
+    return;
+  }
+
+  const lang = navigator.languages.length
+    ? navigator.languages
+    : document.documentElement.lang;
+
+  const dateFormatter = new Intl.DateTimeFormat(lang, {
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+  });
+
+  Array.prototype.forEach.call(replaceElements, function (elem) {
+    try {
+      const date = Date.parse(elem.dateTime);
+      elem.innerHTML = dateFormatter.format(date);
+    } catch (e) {
+      // skip
+    }
+  });
+})();
