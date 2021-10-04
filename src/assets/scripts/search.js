bba116a1cd5cfb84bc9d61f73c3c8fd4843b681d
Devon Govett <devongovett@gmail.com>
2021-10-03 22:08:01 -0700

    Add search and links to homepage

--- src/assets/scripts/search.js
+++ src/assets/scripts/search.js
@@ -1,7 +1,7 @@
 // When the search input is focused, add a class to the header.
 // This is used to hide some other elements and expand the search bar.
 let search = document.querySelector('#search-input');
-let header = document.querySelector('.parcel-docs-header');
+let header = document.querySelector('header');
 search.onfocus = () => {
   header.classList.add('search');
 };
