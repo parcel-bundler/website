63f8b7f61528b28c8a852c09c54047bcd11e5ff5
Jasper De Moor <jasperdemoor@gmail.com>
2021-11-28 22:00:26 +0100

    migrate to new docsearch infra (#979)

--- src/assets/scripts/search.js
+++ src/assets/scripts/search.js
@@ -1,7 +1,7 @@
 // When the search input is focused, add a class to the header.
 // This is used to hide some other elements and expand the search bar.
-let search = document.querySelector('#search-input');
-let header = document.querySelector('header');
+let search = document.querySelector("#search-input");
+let header = document.querySelector("header");
 search.onfocus = () => {
   header.classList.add("search");
 };
@@ -11,7 +11,8 @@ search.onblur = () => {
 };
 
 docsearch({
-  apiKey: "8b6be780425a72d1a1683abea2636778",
+  appId: "BG2BI65LT3",
+  apiKey: "6309d86dabd1e4f3de34109753166903",
   indexName: "parceljs",
   inputSelector: "#search-input",
   algoliaOptions: {
